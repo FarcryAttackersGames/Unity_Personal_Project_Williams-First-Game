e93af578346535750bca34beabf5dbacb04b0cab
FarcryAttackersGames <farcry5attackersgames132@gmail.com>
2024-09-25 13:40:31 -0700

    Started adding a new enemy, changed the bullet model a tad, and replaced the health pickup to a actual health symbol.
    currently the healthpickup is broken and the script for the new enemy is currently as of right now incomplete.

---
+++ Project Intro/Assets/Scripts/TankEnemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankEnemy : MonoBehaviour
+{
+    [Header("EnemyStats")]
+    public int maxhealth = 5;
+    public int health = 5;
+    public float damagetaken = 0.5f;
+    public int damagedealt = 2;
+    public float knockbackResistance = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
