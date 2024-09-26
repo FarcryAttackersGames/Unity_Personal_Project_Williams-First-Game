359034753280973994a14296bd56c6491452e7dd
FarcryAttackersGames <farcry5attackersgames132@gmail.com>
2024-09-26 13:32:24 -0700

    started working on a model on the basic weapon, and getting an idea for the 1st level of the game.

--- Project Intro/Assets/Scripts/TankEnemy.cs
+++ Project Intro/Assets/Scripts/TankEnemy.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TankEnemy : MonoBehaviour
 {
+    public PlayerController player;
+    public NavMeshAgent agent;
+
     [Header("EnemyStats")]
     public int maxhealth = 5;
-    public int health = 5;
+    public float health = 5f;
     public float damagetaken = 0.5f;
-    public int damagedealt = 2;
+    public int damagedealt = 0;
     public float knockbackResistance = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = player.transform.position;
+
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+        private void OnCollisionEnter(Collision collision)
+    {
+            if (collision.gameObject.tag == "shot");
+            {
+                health -= damagetaken;
+                Destroy(collision.gameObject);
+            }
     }
 }
